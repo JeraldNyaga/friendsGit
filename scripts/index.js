5a0a8f8ddb2e790390ab45f22ef59a8327579fc5
pauline muthoni <kinyanjuipauline69@gmail.com>
2025-01-30 14:50:52 +0300

    initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -25,8 +25,45 @@ const addNum = nums2.map((value) => {
 console.log(addNum);
 
 //reduce method used to aggregrate use it to get the sum in an array
-const sum = nums.reduce((prev, current) => {
-  return prev + current;
-}, 0);
+// const sum = nums.reduce((prev, current) => {
+//   console.log("here")
 
-console.log(sum);
+//   console.log(current);
+//   console.log((prev[index]));
+// }, 0);
+
+// console.log(sum);
+//muthoni code
+let names = ["stacey", "afasan", "carl", "polly"];
+names.forEach(function (name, index) {
+  console.log(name);
+  console.log(index);
+});
+let cities = ["cape town", "canada", "jamaica", "zanzibar"];
+cities.forEach((city, index) => {
+  console.log(city);
+  console.log(index);
+});
+
+const polly = names.filter((name, index) => {
+  if (index % 2 !== 0) {
+    console.log(name);
+  }
+});
+let fullname = names.map((names) => {
+  console.log(names + " Wamaitha");
+});
+let myNumber = [4, 6, 8, 5, 0, 3, 72, 1000, 56, 375];
+
+
+const bigNum = myNumber.reduce((prev, next) => {
+  let bigNumber = undefined;
+
+  if (prev > next) {
+    bigNumber = prev;
+  } else if (prev < next) {
+    bigNumber = next;
+  }
+  return bigNumber;
+}, 0);
+console.log(bigNum);
